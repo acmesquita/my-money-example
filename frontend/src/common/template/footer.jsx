9bff8c35f94703462ca7e1338d299a2d59655015
Catharina Mesquita <catha.ana.1994@gmail.com>
2018-03-17 14:55:43 -0300

    Adding Footer Component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => (
+    <footer className='main-footer'>
+        <strong>
+            Catharina Mesquita &copy; - 2018
+            <a href='https://github.com/acmesquita' target='_blank'> Github</a>.
+        </strong>
+    </footer>
+)
