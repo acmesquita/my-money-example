f1df982856266c3f10356f5794a233615a267ac9
Catharina Mesquita <catha.ana.1994@gmail.com>
2018-04-29 20:51:47 -0300

    Adding tabContent

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -6,6 +6,7 @@ import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabsContent from '../common/tab/tabsContent'
+import TabContent from '../common/tab/tabContent'
 
 class BillingCycle extends Component {
     render(){
@@ -15,13 +16,16 @@ class BillingCycle extends Component {
                 <Content>
                     <Tabs>
                         <TabsHeader>
-                            <TabHeader label="Listar" icon='bars' target="tabList" />
-                            <TabHeader label="Incluir" icon='plus' target="tabCreate" />
-                            <TabHeader label="Alterar" icon='pencil' target="tabUpdate" />
-                            <TabHeader label="Excluir" icon='trash-o' target="tabDelete" />
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Criar</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Atulizar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
